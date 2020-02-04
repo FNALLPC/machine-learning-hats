ed656a11454da6ac0e856cc0bf8151bc8ae23e6a
Nhan Tran <nhan.v.tran@gmail.com>
2020-02-04 13:33:51 -0600

    add conda setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "ML tutorial setup script"
+
+
+conda create --name MLtutorial2 python=3.7.0
+
+source activate MLtutorial2
+
+pip install jupyter
+pip install uproot
+pip install h5py keras tensorflow
+pip install pandas matplotlib
+pip install sklearn
